e8963714b444e1ce5e24603a9c14869be968e0e0
ne-el-al <e.a.negreeva@gmail.com>
2025-10-13 23:31:30 +0100

    write out requests template from python app to node app

--- index.js
+++ index.js
@@ -4,10 +4,34 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+    // redirect to /main
 })
 
+app.get("/main", (req, res) => {
+    // render main_page.html
+})
 
+/* 
+app.post("/main", (req, res) => {
+    // insert order to db (to delete)
+})
+*/
+
+app.get("/contacts", (req, res) => {
+    // render contacts.html
+})
+
+app.get("/order", (req, res) => {
+    // render order.html
+})
+
+app.get("/comments", (req, res) => {
+    // render comments.html
+})
+
+app.post("/comments", (req, res) => {
+    // publish coment and stay on the same page
+})
 
 app.listen(port, () => {
     console.log("Server running on port 3000.");
